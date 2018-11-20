54b6296292f6fa4c212944f758c8f308234ee18d
FrostyGhost <frostyghost33@gmail.com>
2018-11-20 17:53:50 +0300

    Code refactoring

---
+++ app/src/main/java/com/example/user/inventoryandroid/NotificationActivity.java
@@ -0,0 +1,17 @@
+package com.example.user.inventoryandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class NotificationActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification_ativity);
+
+
+        //Перегляд взятих книг, поки нема бази, ніцчого писати )
+
+    }
+}
