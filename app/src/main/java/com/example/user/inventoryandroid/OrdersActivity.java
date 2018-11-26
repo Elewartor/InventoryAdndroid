4da179a0407dd5c1fe83d166d62356281d10afc7
FrostyGhost <frostyghost33@gmail.com>
2018-11-26 03:30:04 +0300

    krasivo

--- app/src/main/java/com/example/user/inventoryandroid/OrdersActivity.java
+++ app/src/main/java/com/example/user/inventoryandroid/OrdersActivity.java
@@ -1,5 +1,7 @@
 package com.example.user.inventoryandroid;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
